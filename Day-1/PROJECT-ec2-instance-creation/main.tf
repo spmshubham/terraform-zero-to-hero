5d3106c11968ba2fb3b506fb2afd794fc17fddd4
spmshubham <32236774+spmshubham@users.noreply.github.com>
2024-03-31 20:55:48 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,19 @@
+
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+#this will verify if terraform has access to aws or not 
+
+resource "aws_instance" "example1" {
+    ami = "ami-080e1f13689e07408"  # Specify an appropriate AMI ID, select the ami id after the reqgion is selected on aws
+#Each region have different ami id for smae instance 
+    instance_type = "t3.nano"
+    subnet_id = "subnet-00dbce57335df685a"
+    #subnet is mandatory of creating instance 
+    key_name = "terraform-aws-key1"
+
 }
+
+
+
